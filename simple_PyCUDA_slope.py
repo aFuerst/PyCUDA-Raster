9b7a5e29015c3228e5cdfdfcc1e7a2df277fe55b
aFuerst <fuersta.2013@gmail.com>
2016-06-17 11:20:11 -0400

    Commenting and change to how neighbors are gathered
    Put in comments for GPU functions. Created a function to handle
    gathering neighbors after found out that current version does not gather
    them properly.

--- simple_PyCUDA_slope.py
+++ simple_PyCUDA_slope.py
@@ -34,45 +34,65 @@ ncols_gpu = cuda.mem_alloc(ncols.nbytes)
 nrows_gpu = cuda.mem_alloc(nrows.nbytes)
 
 # transfer data to GPU
-data_gpu = htod(data_gpu, data)
-result_gpu = htod(result_gpu, result)
-ppt_gpu = htod(ppt_gpu, pixels_per_thread)
-NODATA_gpu = htod(NODATA_gpu, NODATA)
-ncols_gpu = htod(ncols_gpu, ncols)
-nrows_gpu = htod(nrows_gpu, nrows)
+htod(data_gpu, data)
+htod(result_gpu, result)
+htod(ppt_gpu, pixels_per_thread)
+htod(NODATA_gpu, NODATA)
+htod(ncols_gpu, ncols)
+htod(nrows_gpu, nrows)
 
+# function(s) to be compiled by CUDA for execution on GPU
 mod = SourceModule(
 """
 #include <math.h>
 
+/************************************************************************************************
+	on-GPU function that gets the neighbors of the pixel at curr_offset
+	stores them in the passed-by-reference array 'store'
+ ************************************************************************************************/
+__device__ getNeighbors(double *store, double *data, unsigned curr_offset, int pixels_per_thread){
+	int i;
+	for(i = -1, i < 2, ++i){
+		store[1 + i] = data[curr_offset + i];
+		store[4 + i] = data[curr_offset + i + 4];
+		store[7 + i] = data[curr_offset + i + 7];
+	}
+}
+
+/************************************************************************************************
+	kernel function to calculate the slope of pixels in 'data' and stores them in 'result'
+	handles a variable number of calculations based on its thread/block location and the size of pixels_per_thread
+	
+	TODO: find a way to use a struct to hold (pixels_per_thread, NODATA, ncols, nrows) to cut down on parameters
+		  Ensure function is actually calculating all pixels and isn;t missing any,
+			unsure if offset calculation is correct
+
+************************************************************************************************/
 __global__ void simple_slope(double *data, double *result, int pixels_per_thread, double NODATA, int ncols, int nrows){
-	/* get thread x,y coordinates */
+	/* get individual thread x,y coordinates */
 	unsigned x = blockIdx.x * blockDim.x + threadIdx.x;
 	unsigned y = blockIdx.y * blockDim.y + threadIdx.y;
 	unsigned offset = x * y * pixels_per_thread;
 
 	int i;
+	/* iterate over assigned pixels and calculate slope for all of them */
 	for(i=offset; i < offset + pixels_per_thread, ++i){
-		/*
+
 		if(data[i] == NODATA){
 			result[i] == NODATA;
 		} else{
 			double nbhd[9];
+			getNeighbors(nbhd, data, i, pixels_per_thread);
 			int q = 0;
 			for(q, q < 9, ++q){
-				if(data[i+q] == NODATA){
-					nbhd[q] = data[i+4];
-				} else {
-					nbhd[q] = data[i+q]; 
+				if(nbhd[q] == NODATA){
+					nbhd[q] = data[i];
 				}
 			}
 			double dz_dx = (nbhd[2] + (2*nbhd[5]) + nbhd[8] - (nbhd[0] + (2*nbhd[3]) + nbhd[6])) / (8*10);
 			double dz_dy = (nbhd[6] + (2*nbhd[7]) + nbhd[8] - (nbhd[0] + (2*nbhd[1]) + nbhd[2])) / (8*10);
-			result[i] = atan(sqrt(pow(dz_dx, 2) + pow(dz_dy, 2)));
-			
+			result[i] = atan(sqrt(pow(dz_dx, 2) + pow(dz_dy, 2)));	
 		}
-		*/
-		result[i] = offset;
 	}
 }
 """)
@@ -82,14 +102,17 @@ func = mod.get_function("slope")
 # call slope function using 5 16x16 blocks pf threads -> 
 func(data_gpu, result_gpu, ppt_gpu, NODATA_gpu, ncols_gpu, nrows_gpu, block = (16, 16, 5))
 
+# copy result back to host
 cuda.memcpy_dtoh(result, result_gpu)
 
-# free on-device memory
+# free on-device memory, not necessary currently; just here to test if it works
+# not sure if we will need to do this when dealing with files bigger than space on GPU
 result_gpu.free()
 data_gpu.free()
 ppt_gpu.free()
 NODATA_gpu.free()
 
+# write result to file
 header_str = ("ncols %s\n"
               "xllcorner %s\n"
               "nrows %s\n"
@@ -98,6 +121,6 @@ header_str = ("ncols %s\n"
               "NODATA_value %d"
               % (ncols, nrows, xllcorner, yllcorner, cellsize, NODATA)
              )
-
-#print slope_data
 np.savetxt("output.asc", result, fmt='%5.2f', header=header_str, comments='')
+
+
