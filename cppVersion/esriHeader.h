7bda40651d57e4d024c8c8b7cb8e9d2760c85763
ckazer <ckazer95@gmail.com>
2016-07-08 14:29:52 -0400

    Fixed struct conflict

--- cppVersion/esriHeader.h
+++ cppVersion/esriHeader.h
@@ -1,8 +1,13 @@
-struct header {
+#ifndef ESRI_HEADER
+#define ESRI_HEADER
+
+typedef struct {
     long long ncols;
     long long nrows;
     long long NODATA;
     double cellsize;
     double xllcorner;
     double yllcorner;
-};
+} esriHeader;
+
+#endif
